bd6ff68dc8ec76977257490e1fff1ae4783ce1d7
Mike Hance <mhance@gmail.com>
2026-03-17 16:49:38 -0500

    update to hadd script

--- hadd_picontuples.sh
+++ hadd_picontuples.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-source $ATLAS_LOCAL_ROOT_BASE/packageSetups/localSetup.sh "root 6.34.04-x86_64-el9-gcc13-opt"
+#source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+#source $ATLAS_LOCAL_ROOT_BASE/packageSetups/localSetup.sh "root 6.34.04-x86_64-el9-gcc13-opt"
 
-af_version="v3"
-nt_version="v3.Y"
+af_version="v4"
+nt_version="v4"
 
 N=5
 
@@ -14,7 +14,7 @@ for inputfiles in ""; do #"_sig" "_mc" "_data"; do
 
     #jsoninput=../light-roast/dataset_runnable/af_${af_version}${inputfiles}.json
     jsoninput=/data/kratsg/radiative-decays/af_${af_version}.json
-    dataset_names=$(jq -r 'keys_unsorted[]' ${jsoninput} | sed s/_mc20[ade]/_mc20/g | sed s/_mc23[ad]/_mc23/g | uniq)
+    dataset_names=$(jq -r 'keys_unsorted[]' ${jsoninput} | sed s/_mc20[ade]/_mc20/g | sed s/_mc23[ade]/_mc23/g | uniq)
 
     for ds in ${dataset_names}; do
 
@@ -24,17 +24,19 @@ for inputfiles in ""; do #"_sig" "_mc" "_data"; do
     
         echo "Working on ${ds}"
 
-	#hadd -f /data/mhance/SUSY/ntuples/v3.1_condor_2/output_${ds}.root /data/mhance/SUSY/ntuples/v3.1_condor_2/output_${ds}*.root &
-	rm -f /data/mhance/SUSY/ntuples/${nt_version}/output_${ds}.root 
+	#rm -f /data/mhance/SUSY/ntuples/${nt_version}/output_${ds}.root 
 	mkdir -p /data/mhance/SUSY/ntuples/${nt_version}/campaigns
-	mv /data/mhance/SUSY/ntuples/${nt_version}/output_${ds}*.root /data/mhance/SUSY/ntuples/${nt_version}/campaigns
+	#mv /data/mhance/SUSY/ntuples/${nt_version}/output_${ds}*.root /data/mhance/SUSY/ntuples/${nt_version}/campaigns
+	#for c in "a" "d" "e"; do
+	#    mv /data/mhance/SUSY/ntuples/${nt_version}/PICOTEST_RAv4_${ds}${c}.root /data/mhance/SUSY/ntuples/${nt_version}/campaigns/output_${ds}${c}.root
+	#done
 	hadd -f /data/mhance/SUSY/ntuples/${nt_version}/output_${ds}.root /data/mhance/SUSY/ntuples/${nt_version}/campaigns/output_${ds}*.root &
 
-	if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
+	#if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
 	    # now there are $N jobs already running, so wait here for any job
 	    # to be finished so there is a place to start next one.
-	    wait -n
-	fi
+	#    wait -n
+	#fi
 
     done
 done
